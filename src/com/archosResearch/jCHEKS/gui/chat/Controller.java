05a35fc28f7597424a43443a544ed25ff2f66499
Michael Roussel <rousselm4@gmail.com>
2015-05-25 14:27:31 -0400

    Create gui and edit Controller

--- src/com/archosResearch/jCHEKS/gui/chat/Controller.java
+++ src/com/archosResearch/jCHEKS/gui/chat/Controller.java
@@ -5,7 +5,6 @@ package com.archosResearch.jCHEKS.gui.chat;
  * @author Michael Roussel <rousselm4@gmail.com>
  */
 public class Controller {
-    public static void Main(){
-    
+    public static void main(String[] args){
     }
 }
