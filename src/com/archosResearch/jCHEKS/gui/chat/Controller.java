9ffb97b32fa7ec72b99fccf57043d3b59622ef61
Michael Roussel <rousselm4@gmail.com>
2015-05-25 14:08:32 -0400

    Create Controller class with Main method

---
+++ src/com/archosResearch/jCHEKS/gui/chat/Controller.java
@@ -0,0 +1,11 @@
+package com.archosResearch.jCHEKS.gui.chat;
+
+/**
+ *
+ * @author Michael Roussel <rousselm4@gmail.com>
+ */
+public class Controller {
+    public static void Main(){
+    
+    }
+}
