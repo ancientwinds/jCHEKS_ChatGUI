32d99cff9644497de375118ac210601238cb091d
Michael Roussel <rousselm4@gmail.com>
2015-05-25 13:52:31 -0400

    Create Contact class

---
+++ src/com/archosResearch/jCHEKS/gui/chat/Contact.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.archosResearch.jCHEKS.gui.chat;
+
+/**
+ *
+ * @author Michael Roussel <rousselm4@gmail.com>
+ */
+public class Contact {
+    private String name;
+    private int senderChaoticSystemId;
+    private int receiverChaoticSystemId;
+    //private AbstractCommunicator communicator;
+    //private AbstractEncrypter encrypter;
+    
+    public Contact(String aName, int aSenderChaoticSystemId, int aReceiverChaoticSystemId){
+        this.name = aName;
+        this.senderChaoticSystemId = aSenderChaoticSystemId;
+        this.receiverChaoticSystemId = aReceiverChaoticSystemId;
+    }
+    
+}
