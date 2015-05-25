1ac994667e3fb1932b44857c674d160f711c6305
Michael Roussel <rousselm4@gmail.com>
2015-05-25 14:45:18 -0400

    Move fxml file in view packade and edit controller to render basic view

--- src/com/archosResearch/jCHEKS/gui/chat/Controller.java
+++ src/com/archosResearch/jCHEKS/gui/chat/Controller.java
@@ -1,10 +1,55 @@
 package com.archosResearch.jCHEKS.gui.chat;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Michael Roussel <rousselm4@gmail.com>
  */
-public class Controller {
-    public static void main(String[] args){
+public class Controller extends Application{
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Chat");
+
+        initRootLayout();
     }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Controller.class.getResource("view/gui.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
