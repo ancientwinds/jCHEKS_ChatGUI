341239c3bd7d4cfffa5c025d18d310011a497f0a
rousselm4 <rousselm4@gmail.com>
2015-05-27 15:52:04 -0400

    Add MessageCollect

---
+++ src/com/archosResearch/jCHEKS/gui/chat/model/MessageCollection.java
@@ -0,0 +1,15 @@
+package com.archosResearch.jCHEKS.gui.chat.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rousselm4
+ */
+public class MessageCollection {
+     private ArrayList<Message> messages;
+    
+    public MessageCollection(){
+        this.messages = new ArrayList<Message>();
+    }
+}
