86125535555e41720715a47a18adeb701ee1b614
Michael Roussel <rousselm4@gmail.com>
2015-05-28 08:30:24 -0400

    Create ContactCollectionTest with 2 tests

---
+++ test/com/archosResearch/jCHEKS/gui/chat/model/ContactCollectionTest.java
@@ -0,0 +1,33 @@
+package com.archosResearch.jCHEKS.gui.chat.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Michael Roussel <rousselm4@gmail.com>
+ */
+public class ContactCollectionTest {
+    
+    @Test
+    public void constructor_should_create_the_contact_list() throws Exception {
+        ContactCollection aContactCollection = null;
+        aContactCollection = new ContactCollection();
+        assertNotNull(aContactCollection);
+    }
+    
+    @Test
+    public void constructor_with_contacts_as_parameters_should_create_the_contact_list(){
+        ContactCollection aContactCollection = null;
+        aContactCollection = new ContactCollection(new Contact("Alice"), new Contact("Bob"));
+        assertNotNull(aContactCollection);
+    }
+    
+    @Test (expected=ContactNotFoundException.class)
+    public void testFindByName() throws ContactNotFoundException{
+        ContactCollection instance = new ContactCollection();
+        String aName = "Alice";
+        Contact result = instance.findByName(aName);
+    }
+    
+}
