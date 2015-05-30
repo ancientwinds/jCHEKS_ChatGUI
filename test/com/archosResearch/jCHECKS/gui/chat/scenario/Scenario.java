ba7e8dbcb1b6b1baef30daa88872b142f6850833
rousselm4 <rousselm4@gmail.com>
2015-05-30 16:20:44 -0400

    Add a scenario that instatiate AppController. Add menubar with a useless element in it to chat.fxml.

---
+++ test/com/archosResearch/jCHECKS/gui/chat/scenario/Scenario.java
@@ -0,0 +1,17 @@
+package com.archosResearch.jCHECKS.gui.chat.scenario;
+
+import com.archosResearch.jCHEKS.gui.chat.AppController;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Michael Roussel <rousselm4@gmail.com>
+ */
+public class Scenario {
+    
+    @Test
+    public void scenario1() {
+        AppController appController = new AppController();
+    }
+}
