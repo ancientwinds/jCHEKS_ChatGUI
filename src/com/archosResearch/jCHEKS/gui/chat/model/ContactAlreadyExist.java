c08d2fa563b7c6b040dfa1b5a1da3128a072d153
rousselm4 <rousselm4@gmail.com>
2015-05-30 13:02:11 -0400

    Add 2 exceptions to add method of ContactCollection: ContactAlreadyExist and NameOfContactAlreadyExistInContacts

---
+++ src/com/archosResearch/jCHEKS/gui/chat/model/ContactAlreadyExist.java
@@ -0,0 +1,7 @@
+package com.archosResearch.jCHEKS.gui.chat.model;
+
+/**
+ *
+ * @author Michael Roussel <rousselm4@gmail.com>
+ */
+class ContactAlreadyExist extends Exception {}
--- src/com/archosResearch/jCHEKS/gui/chat/model/ContactCollection.java
+++ src/com/archosResearch/jCHEKS/gui/chat/model/ContactCollection.java
@@ -1,27 +1,35 @@
 package com.archosResearch.jCHEKS.gui.chat.model;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+
 /**
  *
  * @author Michael Roussel <rousselm4@gmail.com>
  */
 public class ContactCollection {
+
     private final ArrayList<Contact> contacts;
-    
+
     public ContactCollection(Contact... contacts) {
         this.contacts = new ArrayList<>();
         this.contacts.addAll(Arrays.asList(contacts));
     }
-    
-    public Contact findByName(String aName) throws ContactNotFoundException{
-        for(Contact contact: this.contacts){
-            if(contact.getName().equals(aName)){ return contact;}
-        }  
+
+    public Contact findByName(String aName) throws ContactNotFoundException {
+        for (Contact contact : this.contacts) {
+            if (contact.getName().equals(aName)) {
+                return contact;
+            }
+        }
         throw new ContactNotFoundException();
     }
 
-    void add(Contact contact) {
-        this.contacts.add(contact);
+    void add(Contact newContact) throws ContactAlreadyExist, NameOfContactAlreadyExistInContacts {
+        if (contacts.contains(newContact)) throw new ContactAlreadyExist();
+        for (Contact aContact : this.contacts) 
+            if (aContact.getName().equals(newContact.getName())) throw new NameOfContactAlreadyExistInContacts();
+        this.contacts.add(newContact);
     }
-    
+
 }
