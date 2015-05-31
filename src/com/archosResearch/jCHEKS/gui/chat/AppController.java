39d5052f59120a1ed6eaaae13ef94d7a0d169d49
rousselm4 <rousselm4@gmail.com>
2015-05-31 17:11:54 -0400

    Extract interface from AppController. Add implementation: AppControllerDefault.

--- src/com/archosResearch/jCHEKS/gui/chat/AppController.java
+++ src/com/archosResearch/jCHEKS/gui/chat/AppController.java
@@ -1,39 +1,10 @@
 package com.archosResearch.jCHEKS.gui.chat;
 
-import com.archosResearch.jCHEKS.gui.chat.model.*;
-import com.archosResearch.jCHEKS.gui.chat.view.JavaFxViewController;
-import com.archosResearch.jCHEKS.gui.chat.view.ViewController;
-import java.util.ArrayList;
-import java.util.HashMap;
-
 /**
  *
  * @author Michael Roussel <rousselm4@gmail.com>
  */
-public class AppController {
-    
-    private final Model model;
-    private final ViewController viewController;
-    //private final CHECKSEngine engine;
-    
-    public AppController(/*CHECKSEngine engine, */Model model, ViewController viewController){
-        this.viewController = viewController;
-        this.viewController.setAppController(this);
-        //this.engine = engine;
-        this.model = model;
-        this.model.addObserver(this.viewController);
-        //this.model.addObserver(engine);
-    }
-    
-    public void handleIncomingMessage(String messageContent, String contactName){
-        this.model.addIncomingMessage(messageContent, contactName);
-    }
-    
-    public void handleOutgoingMessage(String messageContent, String contactName){
-        this.model.addOutgoingMessage(messageContent, contactName);
-    }
-    
-    public static void main(String args[]){
-        new AppController(/* new CHECKSEngine(),*/new ModelDefault(new ContactCollectionDefault(new ArrayList()), new HashMap<>(), new ArrayList()), JavaFxViewController.getInstance());
-    }
+public interface AppController {
+    public void handleIncomingMessage(String messageContent, String contactName);
+    public void handleOutgoingMessage(String messageContent, String contactName);
 }              
---
+++ src/com/archosResearch/jCHEKS/gui/chat/AppControllerDefault.java
@@ -0,0 +1,39 @@
+package com.archosResearch.jCHEKS.gui.chat;
+
+import com.archosResearch.jCHEKS.gui.chat.model.*;
+import com.archosResearch.jCHEKS.gui.chat.view.JavaFxViewController;
+import com.archosResearch.jCHEKS.gui.chat.view.ViewController;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Michael Roussel <rousselm4@gmail.com>
+ */
+public class AppControllerDefault implements AppController{
+    
+    private final Model model;
+    private final ViewController viewController;
+    //private final CHECKSEngine engine;
+    
+    public AppControllerDefault(/*CHECKSEngine engine, */Model model, ViewController viewController){
+        this.viewController = viewController;
+        this.viewController.setAppController(this);
+        //this.engine = engine;
+        this.model = model;
+        this.model.addObserver(this.viewController);
+        //this.model.addObserver(engine);
+    }
+    
+    public void handleIncomingMessage(String messageContent, String contactName){
+        this.model.addIncomingMessage(messageContent, contactName);
+    }
+    
+    public void handleOutgoingMessage(String messageContent, String contactName){
+        this.model.addOutgoingMessage(messageContent, contactName);
+    }
+    
+    public static void main(String args[]){
+        new AppControllerDefault(/* new CHECKSEngine(),*/new ModelDefault(new ContactCollectionDefault(new ArrayList()), new HashMap<>(), new ArrayList()), JavaFxViewController.getInstance());
+    }
+}              
