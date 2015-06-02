1df5c10660ea8ddab83a47d5ceaac5b7c2f98953
Michael Roussel <rousselm4@gmail.com>
2015-06-02 08:20:02 -0400

    Add test for addMessage in MessageCollectionTest.

--- test/com/archosResearch/jCHEKS/gui/chat/model/MessageCollectionTest.java
+++ test/com/archosResearch/jCHEKS/gui/chat/model/MessageCollectionTest.java
@@ -12,18 +12,30 @@ public class MessageCollectionTest {
 
     @Test
     public void constructor_should_create_the_message_collection() throws Exception {
-        MessageCollection aMessageCollection = null;
-        aMessageCollection = new MessageCollectionDefault(new ArrayList());
-        assertNotNull(aMessageCollection);
+        MessageCollection messageCollection = null;
+        messageCollection = new MessageCollectionDefault(new ArrayList());
+        assertNotNull(messageCollection);
     }
-    
+
     @Test
     public void getAllMessage_should_return_a_void_arrayList_if_there_is_no_message() throws Exception {
         ArrayList<Message> exepectedResult = new ArrayList();
-        MessageCollection aMessageCollection = new MessageCollectionDefault(exepectedResult);
+        MessageCollection messageCollection = new MessageCollectionDefault(exepectedResult);
         ArrayList<Message> result = null;
-        result = aMessageCollection.getAllMessages();
+        result = messageCollection.getAllMessages();
         assertEquals(result, exepectedResult);
     }
-    
+
+    @Test
+    public void addMessage_should_add_a_message_in_the_message_collection() throws Exception {
+        ArrayList<Message> exepectedResult = new ArrayList();
+        Message messageToAdd = new OutgoingMessage("This is a test message.");
+        exepectedResult.add(messageToAdd);
+        MessageCollection messageCollection = new MessageCollectionDefault(exepectedResult);
+        messageCollection.add(messageToAdd);
+        ArrayList<Message> result = null;
+        result = messageCollection.getAllMessages();
+        assertEquals(result, exepectedResult);
+    }
+
 }
