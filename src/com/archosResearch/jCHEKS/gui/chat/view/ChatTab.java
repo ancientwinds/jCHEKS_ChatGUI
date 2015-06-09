163f56b38cf4bf3dfaea846189c525aa73cf1008
Michael Roussel <rousselm4@gmail.com>
2015-06-09 15:17:08 -0400

    Add ChatTab Class to help manage the tab. Each tab represent one conversation.

---
+++ src/com/archosResearch/jCHEKS/gui/chat/view/ChatTab.java
@@ -0,0 +1,48 @@
+package com.archosResearch.jCHEKS.gui.chat.view;
+
+import javafx.scene.control.*;
+import javafx.scene.input.*;
+import javafx.scene.layout.*;
+
+/**
+ *
+ * @author Michael Roussel <rousselm4@gmail.com>
+ */
+public class ChatTab extends Tab{
+    
+    TextField inputField;
+    TextArea outputField;
+    
+    public ChatTab(String contactName){
+        super(contactName);
+        
+        VBox mainContainer = new VBox();
+
+        outputField = new TextArea();
+        outputField.setEditable(false);
+        mainContainer.getChildren().add(outputField);
+        
+        inputField = new TextField();
+        inputField.setOnKeyPressed((KeyEvent event) -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                displayMessage(inputField.getText());
+                resetInputField();
+                this.setText(new StringBuilder(this.getText()).reverse().toString());
+                //TODO call javaFxViewController
+            }
+        });
+        
+        mainContainer.getChildren().add(inputField);
+        this.setContent(mainContainer);
+        this.setClosable(true);
+    }
+    
+    public void displayMessage(String message){
+        outputField.appendText(message+"\n");
+    }
+    
+    public void resetInputField(){
+        inputField.setText("");
+    }
+    
+}
