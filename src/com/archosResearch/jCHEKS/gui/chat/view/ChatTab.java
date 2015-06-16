a00b6ca20a943d097da25f3ef46cb55dc40b4cb6
Michael Roussel <rousselm4@gmail.com>
2015-06-16 09:51:01 -0400

    Remove old img, add img, and put them in res/img. Clean up in ChatTab. Changer ArrayList to List.

--- src/com/archosResearch/jCHEKS/gui/chat/view/ChatTab.java
+++ src/com/archosResearch/jCHEKS/gui/chat/view/ChatTab.java
@@ -1,111 +1,108 @@
 package com.archosResearch.jCHEKS.gui.chat.view;
 
 import com.archosResearch.jCHEKS.concept.engine.message.AbstractMessage;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
 import javafx.scene.layout.*;
-import javafx.util.Callback;
 
 /**
  *
  * @author Michael Roussel <rousselm4@gmail.com>
  */
-public class ChatTab extends Tab{
-    
+public class ChatTab extends Tab {
+
     TextField inputField;
-    ListView<AbstractMessage>  messagesListView;
+    ListView<AbstractMessage> messagesListView;
     MessageListCell msgList;
-    
-    public ChatTab(String contactName){
+
+    public ChatTab(String contactName) {
         super(contactName);
-        VBox mainContainer = new VBox();
-        messagesListView = new ListView();
-        
-        ObservableList<AbstractMessage> data = FXCollections.observableArrayList();
-        messagesListView.setItems(data);
-        messagesListView.setCellFactory((ListView<AbstractMessage> param) -> new MessageListCell());
-        
-        mainContainer.getChildren().add(messagesListView);
-        /*
-        outputField = new TextArea();
-        outputField.setEditable(false);
-        mainContainer.getChildren().add(outputField);
-        */
-        
-        inputField = new TextField();
-        inputField.setOnKeyPressed((KeyEvent event) -> {
+        this.messagesListView = createOutputNode();
+        this.inputField = createInputNode();
+        this.setContent(this.createChatContainer());
+        this.setClosable(true);
+    }
+
+    public void displayMessage(AbstractMessage message) {
+        Platform.runLater(() -> this.messagesListView.getItems().add(message));
+    }
+
+    private void resetInputField() {
+        inputField.setText("");
+    }
+    
+    public void refresh() {
+        ObservableList<AbstractMessage> items = this.messagesListView.getItems();
+        this.messagesListView.setItems(null);
+        this.messagesListView.setItems(items);
+    }
+    
+    private ListView<AbstractMessage> createOutputNode(){
+        ListView<AbstractMessage> outputNode = new ListView();
+        outputNode.setItems(FXCollections.<AbstractMessage>emptyObservableList());
+        outputNode.setCellFactory((ListView<AbstractMessage> param) -> new MessageListCell());
+        return outputNode;
+    }
+    private TextField createInputNode(){
+        TextField inputNode = new TextField();
+        inputNode.setOnKeyPressed((KeyEvent event) -> {
             if (event.getCode() == KeyCode.ENTER) {
                 JavaFxViewController.getInstance().forwardOutgoingMessage(inputField.getText(), this.getText());
                 resetInputField();
             }
         });
-        
-        mainContainer.getChildren().add(inputField);
-        this.setContent(mainContainer);
-        this.setClosable(true);
+        return inputNode;
     }
     
-    public void displayMessage(AbstractMessage message){
-        Platform.runLater(() -> this.messagesListView.getItems().add(message));
-        
-        /*try {
-            Thread.sleep(5000);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(ChatTab.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        message.setContent("Patate");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(ChatTab.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        message.setContent("poutine");*/
+    private Node createChatContainer(){
+        VBox chatContainer = new VBox();
+        chatContainer.getChildren().add(this.messagesListView);
+        chatContainer.getChildren().add(this.inputField);
+        return chatContainer;
     }
     
-    private void resetInputField(){
-        inputField.setText("");
-    }
     private static class MessageListCell extends ListCell<AbstractMessage> {
-        
-            @Override
-            public void updateItem(AbstractMessage message, boolean empty) {
-                super.updateItem(message, empty);
-                if (empty) {
-                    setGraphic(null);
-                    setText(null);
-                } else {
-                    String src;
-                    switch(message.getState()){
-                        case FOR_ME:
-                            src = "glyphicons-246-chat.png";
-                            break;
-                        default:
-                            src = "glyphicons-83-roundabout.png";
-                    }
-                    Image fxImage = new Image(JavaFxViewController.class.getResourceAsStream(src));
-                    ImageView imageView = new ImageView(fxImage);
-                    setGraphic(imageView);
-                    setText(message.getContent());
+
+        @Override
+        public void updateItem(AbstractMessage message, boolean empty) {
+            super.updateItem(message, empty);
+            if (empty) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                String src;
+                switch (message.getState()) {
+                    case FOR_ME:
+                        src = "123.png";
+                        break;
+                    case WAITING_FOR_ACK:
+                        src = "129.png";
+                        break;
+                    case WAITING_FOR_SECURE_ACK:
+                        src = "128.png";
+                        break;
+                    case OK:
+                        src = "121.png";
+                        break;
+                    case FAILED:
+                        src = "127.png";
+                        break;
+                    default:
+                        src = "127.png";
                 }
-            }
-            
-            /*@Override
-            public void commitEdit(String newValue){
-                Image fxImage = new Image("glyphicons-83-roundabout.png");
+                Image fxImage = new Image(JavaFxViewController.class.getResourceAsStream("res/img/" + src));
                 ImageView imageView = new ImageView(fxImage);
                 setGraphic(imageView);
-            }*/
+                setText(message.getContent());
+            }
         }
-    public void refresh(){
-        ObservableList<AbstractMessage> items = this.messagesListView.getItems();
-        this.messagesListView.setItems(null);
-        this.messagesListView.setItems(items);
     }
+
+
 }
