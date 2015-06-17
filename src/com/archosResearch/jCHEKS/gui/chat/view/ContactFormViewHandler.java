8d9fea6133e967b9d87e4313fe7f633191b76e89
Michael Roussel <rousselm4@gmail.com>
2015-06-17 11:42:52 -0400

    Add validation to contactForm.

--- src/com/archosResearch/jCHEKS/gui/chat/view/ContactFormViewHandler.java
+++ src/com/archosResearch/jCHEKS/gui/chat/view/ContactFormViewHandler.java
@@ -32,6 +32,9 @@ public class ContactFormViewHandler {
     private ChoiceBox encrypterField;
     
     @FXML
+    private Button createContactButton;
+    
+    @FXML
     private void handleCreateContact() {
         ContactInfo contactInfo = new ContactInfo(addressField.getText(), Integer.parseInt(portField.getText()), nameField.getText(), uniqueId.getText());
         JavaFxViewController.getInstance().sendNewContactRequest(contactInfo);
@@ -52,6 +55,28 @@ public class ContactFormViewHandler {
            for (ContactInfo contactInfo : contactsInfo) {
                 controller.sendNewContactRequest(contactInfo);
            }
+        }
+    }
+    
+    @FXML
+    private void validate(){
+        boolean ipIsValid = addressField.getText().matches("^(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)(\\.(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)){3}$");
+        boolean portIsValid = portField.getText().matches("^0*(?:6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9])$");
+        if(ipIsValid){
+            addressField.setStyle("-fx-text-inner-color: black;");
+        }else {
+            addressField.setStyle("-fx-text-inner-color: red;");
+        }
+        
+        if(portIsValid){
+            portField.setStyle("-fx-text-inner-color: black;");
+        }else {
+            portField.setStyle("-fx-text-inner-color: red;");
+        }
+        if(ipIsValid & portIsValid){
+            createContactButton.setDisable(false);
+        }else{
+            createContactButton.setDisable(true);
         }
     }
 
