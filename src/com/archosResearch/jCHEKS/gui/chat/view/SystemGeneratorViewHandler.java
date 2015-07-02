a5472369b9cc8fb6fde6306547436788d784309c
thomas <thomas@ubuntu>
2015-07-02 11:35:26 -0400

    System Generator view created.

---
+++ src/com/archosResearch/jCHEKS/gui/chat/view/SystemGeneratorViewHandler.java
@@ -0,0 +1,16 @@
+package com.archosResearch.jCHEKS.gui.chat.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author thomas
+ */
+public class SystemGeneratorViewHandler {
+    
+    @FXML
+    private TextField keyLengthField;
+    @FXML
+    private TextField systemIdField;
+}
